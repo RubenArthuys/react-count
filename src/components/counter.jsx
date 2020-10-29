dced910058d5d9f1912f4884acd64db06f478a0e
RubenArthuys <ruben.arthuys@gmail.com>
2020-10-29 17:20:42 +0100

    new

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,11 +3,10 @@ import React, { Component } from 'react';
 class Counter extends Component {
   state = {
     count: 0,
-    // tags: ['tag1', 'tag2', 'tag3']
   };
 
-  handleIncrement() {
-    console.log('increment clicked')
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
   }
 
   render() { 
@@ -15,11 +14,10 @@ class Counter extends Component {
       //React.Fragment to delete div - won't appear in console Elements
       <div>
         <span className={ this.getBadgeClasses() }>{ this.formatCount() }</span>
-        <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
-
-        {/* <ul>
-          { this.state.tags.map(tag => <li key={tag}> { tag } </li>) }
-        </ul> */}
+        <button 
+          onClick={ () => this.handleIncrement()} 
+          className="btn btn-secondary btn-sm">Increment
+        </button>
       </div>
     );
   }
