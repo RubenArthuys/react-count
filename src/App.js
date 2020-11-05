0b9a590caf89a023f7618adcd2c2bd64929a721c
RubenArthuys <ruben.arthuys@gmail.com>
2020-11-05 17:23:21 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-// import logo from './logo.svg';
+import NavBar from './components/navbar';
+import Counters from './components/counters';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      hey there
-    </div>
+    <React.Fragment>
+      <NavBar />
+      <main className="container">
+        <Counters />
+      </main>
+    </React.Fragment>
   );
 }
 
